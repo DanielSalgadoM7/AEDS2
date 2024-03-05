d8aa1d4187b95250dae9901e623d4be2ee40e4f8
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2024-03-04 21:44:03 -0300

    Solução do problema TP1Q06.java

--- Trabalho Prático 1/TP1Q06.java
+++ Trabalho Prático 1/TP1Q06.java
@@ -6,6 +6,8 @@ Na saída padrão, para cada linha de entrada, escreva outra de saída da seguin
 booleano indicando se a é entrada é: composta somente por vogais (X1); composta somente somente por consoantes (X2); 
 um número inteiro (X3); um número real (X4). Se Xi for verdadeiro, seu valor será SIM, caso contrário, NÃO.*/
 
+//Daniel Salgado Magalhães - 821429
+
 public class TP1Q06{
     public static void main(String[] args){
         String entrada;
@@ -17,10 +19,13 @@ public class TP1Q06{
             }
 
             MyIO.print(isVogal(entrada)?"SIM":"NAO");
-            MyIO.println(isConsoante(entrada)?"SIM":"NAO");
+            MyIO.print(isConsoante(entrada)?" SIM":" NAO");
+            MyIO.print(isInteiro(entrada)?" SIM":" NAO");
+            MyIO.println(isReal(entrada)?" SIM":" NAO");
         }
     }
 
+    //função para verificação de palavras com apenas vogais
     public static boolean isVogal(String entrada){
         entrada = entrada.toLowerCase();
         for(int i = 0;i<entrada.length();i++){
@@ -31,12 +36,43 @@ public class TP1Q06{
         return true;
     }
 
+    //função para verificação de palavras com apenas consoantes
     public static boolean isConsoante(String entrada){
         entrada = entrada.toLowerCase();
         for(int i=0;i<entrada.length();i++){
-            if(entrada.charAt(i) == 'a' || entrada.charAt(i) == 'e' || entrada.charAt(i) == 'i' || entrada.charAt(i) == 'o' || entrada.charAt(i) == 'u'){
+            if(entrada.charAt(i) == 'a' || entrada.charAt(i) == 'e' || entrada.charAt(i) == 'i' || entrada.charAt(i) == 'o' || entrada.charAt(i) == 'u' || entrada.charAt(i)>=48 && entrada.charAt(i)<=57){
                 return false;
             }
+        }
+        return true;
+    }
+
+    //função para verificação de palavras com apenas números inteiros
+    public static boolean isInteiro(String entrada){
+        for(int i=0;i<entrada.length();i++){
+            if(!(entrada.charAt(i)>=48 && entrada.charAt(i)<=57)){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    //função para verificação de palavras com apenas números reais
+    public static boolean isReal(String entrada){
+        int cont = 0;
+        for(int i=0;i<entrada.length();i++){
+            if(!(entrada.charAt(i)>=48 && entrada.charAt(i)<=57) && entrada.charAt(i) != '.' || entrada.charAt(i) != ','){
+                return false;
+            }
+            //após a verificação de que é um número de 0 a 9 ou '.' ou ',', ele confere se é um dos dos pontos e realiza o contador
+            if(entrada.charAt(i) == ',' || entrada.charAt(i) == '.'){
+                cont++;
+            }
+        }
+
+        //caso o contador seja maior que 1, a pontuação está errada no número, e ele não é real
+        if(cont>1){
+            return false; 
         }
         return true;
     }
