73793cfd8ed62a36cbbe3b2d858a592a6586cc3c
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2024-03-15 11:19:22 -0300

    Solução do problema TP1Q15.java

--- Trabalho Prático 1/TP1Q15.java
+++ Trabalho Prático 1/TP1Q15.java
@@ -9,23 +9,84 @@ um número inteiro (X3); um número real (X4). Se Xi for verdadeiro, seu valor s
 //Daniel Salgado Magalhães - 821429
 
 public class TP1Q15 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        String entrada;
 
+        while (true) {
+            entrada = MyIO.readLine();
+            if (entrada.equals("FIM")) {
+                break;
+            }
+            MyIO.print(isVogal(entrada) ? "SIM " : "NAO ");
+            MyIO.print(isConsoante(entrada) ? "SIM " : "NAO ");
+            MyIO.print(isInteiro(entrada) ? "SIM " : "NAO ");
+            MyIO.println(isReal(entrada) ? "SIM" : "NAO");
+        }
     }
 
-    static boolean isVogal(String entrada){
-        
+    // métodos recursivos para verificar se só tem vogais
+    static boolean isVogal(String entrada) {
+        return isVogal(entrada, 0);
     }
 
-    static boolean isConsoante(String entrada){
-
+    static boolean isVogal(String entrada, int pos) {
+        entrada = entrada.toLowerCase();
+        if (pos >= entrada.length()) {
+            return true;
+        } else if (entrada.charAt(pos) != 'a' && entrada.charAt(pos) != 'e' && entrada.charAt(pos) != 'i'
+                && entrada.charAt(pos) != 'o' && entrada.charAt(pos) != 'u') {
+            return false;
+        }
+        return isVogal(entrada, pos + 1);
     }
 
-    static boolean isInteiro(String entrada){
-
+    // métodos recursivos para verificar se só tem consoantes
+    static boolean isConsoante(String entrada) {
+        return isConsoante(entrada, 0);
     }
 
-    static boolean isReal(String entrada){
-        
+    static boolean isConsoante(String entrada, int pos) {
+        entrada = entrada.toLowerCase();
+        if (pos >= entrada.length()) {
+            return true;
+        } else if ((entrada.charAt(pos) == 'a' || entrada.charAt(pos) == 'e' || entrada.charAt(pos) == 'i'
+                || entrada.charAt(pos) == 'o' || entrada.charAt(pos) == 'u'
+                || entrada.charAt(pos) >= 48 && entrada.charAt(pos) <= 57)) {
+            return false;
+        }
+        return isConsoante(entrada, pos + 1);
+    }
+
+    // métodos recursivos para verificar se só tem números inteiros
+    static boolean isInteiro(String entrada) {
+        return isInteiro(entrada, 0);
+    }
+
+    static boolean isInteiro(String entrada, int pos) {
+        entrada = entrada.toLowerCase();
+        if (pos >= entrada.length()) {
+            return true;
+        } else if (!(entrada.charAt(pos) >= 48 && entrada.charAt(pos) <= 57)) {
+            return false;
+        }
+        return isInteiro(entrada, pos + 1);
+    }
+
+    // métodos recursivos para verificar se só tem números reais
+    static boolean isReal(String entrada) {
+        return isReal(entrada, 0, 0);
+    }
+
+    static boolean isReal(String entrada, int pos, int cont) {
+        entrada = entrada.toLowerCase();
+        if (pos >= entrada.length()) {
+            return true;
+        } else if (cont > 1|| !((entrada.charAt(pos) >= 48 && entrada.charAt(pos) <= 57) || entrada.charAt(pos) == ',' || entrada.charAt(pos) == '.')) {
+            return false;//retorna falso se não for uma letra ou ',' ou '.'.
+        } else if(entrada.charAt(pos) == ',' || entrada.charAt(pos) == '.'){
+            return isReal(entrada, pos+1, cont+1); //se é ',' ou '.', retorna o método incrementando pos e cont
+        }else{
+            return isReal(entrada, pos + 1, cont); //senão, retorna incrementando apenas a pos
+        }
     }
 }
