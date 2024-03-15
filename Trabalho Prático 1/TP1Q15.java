4a56951821307cda097c95993547b8500206adbe
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2024-03-15 01:07:54 -0300

    Create TP1Q15.java

---
+++ Trabalho Prático 1/TP1Q15.java
@@ -0,0 +1,31 @@
+/*Crie um método recursivo que recebe uma string e retorna true se a mesma é composta somente por vogais. 
+Crie outro método recursivo que recebe uma string e retorna true se a mesma é composta somente por consoantes. 
+Crie um terceiro método recursivo que recebe uma string e retorna true se a mesma corresponde a um número inteiro. 
+Crie um quarto método recursivo que recebe uma string e retorna true se a mesma corresponde a um número real. 
+Na saída padrão, para cada linha de entrada, escreva outra de saída da seguinte forma X1 X2 X3 X4 onde cada Xi é um 
+booleano indicando se a é entrada é: composta somente por vogais (X1); composta somente somente por consoantes (X2); 
+um número inteiro (X3); um número real (X4). Se Xi for verdadeiro, seu valor será SIM, caso contrário, NÃO.*/
+
+//Daniel Salgado Magalhães - 821429
+
+public class TP1Q15 {
+    public static void main(String[] args){
+
+    }
+
+    static boolean isVogal(String entrada){
+        
+    }
+
+    static boolean isConsoante(String entrada){
+
+    }
+
+    static boolean isInteiro(String entrada){
+
+    }
+
+    static boolean isReal(String entrada){
+        
+    }
+}
